9aa72e97c76e65b5006c0b11880b050759ed60e3
MSpezia <matheussflorenz@gmail.com>
2024-10-09 15:40:53 -0300

    Inimigo anda

--- scripts/enemy_base.gd
+++ scripts/enemy_base.gd
@@ -1,20 +1,28 @@
 class_name EnemyBase extends CharacterBody3D
 
-enum EnemyState {IDLE}
+enum EnemyState {IDLE, WALK}
 
 @export var strenght = 5
 @export var hp = 30
-@export var speed = 0.65
+@export var speed = 0.5
+var gravity : float = 9.8
+var death : bool
+var walk_timer : float
+var face_right : bool
+var animation : String
 @onready var animated_sprite : AnimatedSprite3D = $AnimatedSprite3D
+@onready var timer_state : Timer = $Timer
+@onready var player : CharacterBody3D = %Player
 var state : EnemyState = EnemyState.IDLE	
 var enter_state : bool = true
 
 func _physics_process(delta: float) -> void:
 	match state:
 		EnemyState.IDLE: _idle()
+		EnemyState.WALK: _walk(delta)
 		
 		
-func _enter_state(animation: String) -> void:
+func _enter_state() -> void:
 	if enter_state:
 		enter_state = false
 		animated_sprite.play(animation)
@@ -26,3 +34,26 @@ func _change_state(new_state: EnemyState) -> void:
 		enter_state = true
 
 func _idle() -> void: pass
+func _walk(delta: float) -> void: pass
+
+func _stop_movement() -> void:
+	velocity.x = 0
+	velocity.z = 0
+	
+	
+func _set_gravity(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		
+
+func _flip() -> void:
+	face_right = true if player.transform.origin.x > transform.origin.x else false
+	if face_right:
+		animated_sprite.flip_h = true
+	else:
+		animated_sprite.flip_h = false
+
+func _set_animation(anim: String) -> void:
+	if animation != anim:
+		animation = anim
+		animated_sprite.play(animation)
--- scripts/goblin.gd
+++ scripts/goblin.gd
@@ -1,11 +1,41 @@
 extends EnemyBase
 
-func _ready() -> void:
-	state = EnemyState.IDLE
-	
 	
 func _idle() -> void:
-	_enter_state("idle")
+	if enter_state:
+		enter_state = false
+		_set_animation("idle")
+		timer_state.wait_time = randf_range(1, 2)
+		timer_state.start()
+		
+		await  timer_state.timeout
+		_change_state(EnemyState.WALK)
+		
 	
-func _walk() -> void:
-	_enter_state("walk")
+func _walk(delta) -> void:
+	if enter_state:
+		enter_state = false
+		timer_state.wait_time = randf_range(2, 4)
+		timer_state.start()
+		
+		await  timer_state.timeout
+		_change_state(EnemyState.IDLE)
+		
+	var target_distance = player.transform.origin - transform.origin
+	velocity.x = target_distance.x / abs(target_distance.x)
+	
+	walk_timer += delta
+	if walk_timer >= randf_range(1,2):
+		walk_timer = 0
+		velocity.z = randi_range(0,1) if transform.origin.z < player.transform.origin.z else randi_range(-1,0)
+		
+	if abs(target_distance.x) < 0.1:
+		velocity.x = 0
+	
+	if not velocity:
+		_set_animation("idle")
+	else:
+		_set_animation("walk")
+		
+	_flip()
+	move_and_slide()
