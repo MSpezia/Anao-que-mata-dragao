8610104132eeda46487de76248f9fccf0e13640b
Gustavo Wallace <Clasherbr@gmail.com>
2024-11-12 15:25:38 -0300

    correções e arte da vida

---
+++ scripts/sombra_goblin.gd
@@ -0,0 +1,12 @@
+extends Sprite3D
+
+@export var goblin : Node3D
+var y_position = 0
+
+func _ready():
+	y_position = global_transform.origin.y
+
+func _process(delta):
+	global_transform.origin.x = goblin.global_transform.origin.x
+	global_transform.origin.z = goblin.global_transform.origin.z
+	global_transform.origin.y = y_position
--- scripts/ui_main.gd
+++ scripts/ui_main.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 class_name UIMain
 
-@onready var health: ProgressBar = $Player/Health
+@onready var health: TextureProgressBar = $Player/Health
 
 func update_health(value : int) -> void:
 	health.value = value
