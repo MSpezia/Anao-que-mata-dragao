2c048d2c6c47eef457df925bc7c37fee91531946
Gustavo Wallace <Clasherbr@gmail.com>
2024-11-20 17:16:18 -0300

    volume

--- scripts/menu_pause.gd
+++ scripts/menu_pause.gd
@@ -3,6 +3,8 @@ extends Control
 func _ready():
 	hide()
 	$menu.pressed.connect(_on_menu_pressed)
+	var initial_volume = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))) * 100
+	$HSlider.value = initial_volume
 
 func continuar():
 	get_tree().paused = false
@@ -30,3 +32,10 @@ func _on_menu_pressed():
 
 func _process(delta):
 	Esc()
+
+func _on_h_slider_value_changed(value: float) -> void:
+	var normalized_volume = value / 100.0
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(normalized_volume))
+
+func linear_to_db(value: float) -> float:
+	return 20 * (log(value) / log(10)) if value > 0 else -80
